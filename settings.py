9da3187cb3098d26b4cc43d51084a41f0c5d1daf
Anirudh Saksena <anen08saks@gmail.com>
2025-07-07 20:13:10 +0530

    Created window, and drew the spaceship on the screen

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """This class stores all the settings for the alien invasion"""
+
+    def __init__(self):
+        """------------Iniitalize the game settings-------"""
+        """----------------Screen Settings-------------------"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (96, 94, 102)
