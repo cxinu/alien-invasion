07ccab9242f2c487326f626ce27545b236edfd6d
Anirudh Saksena <anen08saks@gmail.com>
2025-07-10 23:37:10 +0530

    Added key event. The ship can now move left and right

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,14 +28,32 @@ class AlienInvasion:
             """
             self._check_events()
             self._update_screen()
+            self.ship.update()
             """Make the most recently drawn screen visible"""
             self.clock.tick(60)
 
     def _check_events(self):
         """Respond to keypresses and events"""
+        """
+        get() collects and returns a list of Event objects currently in the event queue.
+        Each item in that list is an instance of Pygame's Event class
+        event is one Event object
+        event.type is an attribute on that Event object telling you what kind of event it is.
+        """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    """Move ship to the right"""
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update the images on the screen, and flip to the new screen"""
--- ship.py
+++ ship.py
@@ -2,7 +2,7 @@ import pygame
 
 
 class Ship:
-    """A class to manage the player ship"""
+    """A class to manage the player's ship"""
 
     def __init__(self, ai_game):
         """Initializing the ship and set its starting position"""
@@ -10,13 +10,22 @@ class Ship:
         self.screen_rect = (
             ai_game.screen.get_rect()
         )  # Store the dimensions of screen so we can place ship correctly
-
+        """Start the ship with no movement, movement flag."""
+        self.moving_right = False
+        self.moving_left = False
         """Load the ship image and get its rect"""
         self.image = pygame.image.load("../alien-invasion/assets/player-ship.bmp")
         self.rect = self.image.get_rect()
 
         """Start each new ship at the bottom center of the screen"""
         self.rect.midbottom = self.screen_rect.midbottom
+
+    def update(self):
+        """Update ships movement based on the movement flag"""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Draw ship at its current location"""
