7f3809785d2c4b06df1de678ef25d7e608c32102
Anirudh Saksena <anen08saks@gmail.com>
2025-07-13 13:35:42 +0530

    The ship can now shoot bulltes

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -18,6 +19,7 @@ class AlienInvasion:
         )
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -29,6 +31,7 @@ class AlienInvasion:
             self._check_events()
             self._update_screen()
             self.ship.update()
+            self.bullets.update()
             """Make the most recently drawn screen visible"""
             self.clock.tick(60)
 
@@ -55,6 +58,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -62,9 +67,16 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update the images on the screen, and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
         pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -4,7 +4,13 @@ class Settings:
     def __init__(self):
         """------------Iniitalize the game settings-------"""
         """----------------Screen Settings-------------------"""
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1600
+        self.screen_height = 900
         self.bg_color = (25, 23, 33)
         self.ship_speed = 1.5
+
+        """Bullet settings"""
+        self.bullet_speed = 2.0
+        self.bullet_width = 3.0
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
